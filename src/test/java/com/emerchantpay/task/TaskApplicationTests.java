ace1475169f8a07f9c77a23f49b8f84f09cb29ab
Khaled Sobhy <khaled.sobhy@musala.com>
2023-03-24 16:46:22 +0200

    initial commit

---
+++ src/test/java/com/emerchantpay/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerchantpay.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
